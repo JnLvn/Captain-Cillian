6681bd8c982cc8c2d7e13dc86fffa12308b220de
Diarmuid Byrne <diarbbyrne@gmail.com>
2016-02-08 00:19:42 +0000

    Added Mesnu navigation skeleton

---
+++ Assets/Scripts/NavigationScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class NavigationScript : MonoBehaviour {
+
+	// Manages the navigations between different scenes
+    public void loadExploreScene() {
+       SceneManager.LoadScene("Explore");
+    }
+
+    public void loadMainMenu() {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void loadLearnScene() {
+        SceneManager.LoadScene("Learn");
+    }
+
+    public void loadCreateScene() {
+        SceneManager.LoadScene("Create");
+    }
+
+    public void loadFunScene() {
+        SceneManager.LoadScene("Fun");
+    }
+}
