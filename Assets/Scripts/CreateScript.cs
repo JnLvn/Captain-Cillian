d7b3db93bce249206a54a5553af798157269dea6
Diarmuid Byrne <diarbbyrne@gmail.com>
2016-02-18 11:56:28 +0000

    Began work on Create scene

---
+++ Assets/Scripts/CreateScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateScript : MonoBehaviour {
+	public GameObject sideBar;
+	private Animator anim;
+	private bool sideBarActive = false;
+
+	void Start() {
+		Time.timeScale = 1;
+		anim = sideBar.GetComponent<Animator>();
+		anim.enabled = false;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		float beginPos = 0f;
+		foreach (Touch t in Input.touches) {
+			if(t.phase == TouchPhase.Began) {
+				beginPos = t.position.x;
+			}
+			if (t.phase == TouchPhase.Ended) {
+				if (!sideBarActive) {
+					slideIn();
+					sideBarActive = true;
+				} else {
+					slideOut();
+					sideBarActive = false;
+				}
+			}
+		}
+	}
+
+	private void slideIn() {
+		//enable the animator component
+		anim.enabled = true;
+		//play the Slidein animation
+		anim.Play("SlideIn");
+		//set the isPaused flag to true to indicate that the game is paused
+	}
+
+	private void slideOut() {
+		//play the SlideOut animation
+		anim.Play("SideBarSlideOut");
+	}
+
+	public void sideBarButton() {
+		if (sideBarActive) {
+			slideOut();
+			sideBarActive = false;
+			Debug.Log("Sliding out");
+			return;
+		} else
+			slideIn();
+			sideBarActive = true;
+			Debug.Log("Sliding in");
+	}
+}
---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour {
+	public Button btnURL;
+	public GameObject pnlURL;
+	private CanvasGroup cgURL;
+	public Button btnLanguage;
+	public Text txtLanguage;
+	private string[] languages = new string[2];
+
+	public void Start() {
+		languages[0] = "English";
+		languages[1] = "Irish";
+		if(PlayerPrefs.GetString("Language") == null) {
+			PlayerPrefs.SetString("Language", languages[0]);
+		}
+
+		cgURL = pnlURL.GetComponent<CanvasGroup>();
+	}
+
+	public void openLink() {
+		Application.OpenURL("http://www.captaincillian.com/");
+    }
+
+	public void setLanguage() {
+		if(PlayerPrefs.GetString("Language") == languages[0]) {
+			PlayerPrefs.SetString("Language", languages[1]);
+		} else {
+			PlayerPrefs.SetString("Language", languages[0]);
+		}
+		txtLanguage.text = PlayerPrefs.GetString("Language");
+		// translate()
+	}
+
+	private void translate() {
+		// Changes language throughout scene when the language is changed
+		
+	}
+
+	// Update is called once per frame
+	public void toggleURLPanel() {
+		// If panel is invisible and button is clicked
+		if (cgURL.alpha == 0) {
+			cgURL.alpha = 1;
+			cgURL.interactable = true;
+			cgURL.blocksRaycasts = true;
+		} else {
+			cgURL.alpha = 0;
+			cgURL.interactable = false;
+			cgURL.blocksRaycasts = false;
+		}
+	}
+}
